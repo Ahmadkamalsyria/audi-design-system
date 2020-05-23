6bc56867d70af8e79981b095cbceb280765f7228
Ahmad Kamal <Ahmad106107@gmail.com>
2020-05-23 17:36:04 +0200

    ausi-design

--- js/init.js
+++ js/init.js
@@ -6,3 +6,20 @@
     $(".dropdown-trigger").dropdown();
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+$(document).ready(function () {
+
+  $(".menu-btn a").click(function () {
+    $(".overlay").fadeToggle(200);
+    $(this).toggleClass('btn-open').toggleClass('btn-close');
+  });
+
+
+  $('.menu .service-lvl a').on('click', function () {
+    $(this).parent().parent().toggleClass('move');
+  });
+
+  $('.menu .service-lvl .sub .return-lvl a').on('click', function () {
+    $(this).parent().parent().parent().parent().removeClass('move');
+  });
+
+});
